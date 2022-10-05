dd862f575bdfd4aa1836017c8ac5ab21cd960089
Your Name <you@example.com>
2022-10-05 00:10:56 -0500

    Semana 5

---
+++ ejemplo1.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form method="post" action="">
+        Ingresar Valor 1:<br>    <input type="text" name="valor1"><br>
+        Ingresar Valor 2:<br>    <input type="text" name="valor2"><br>
+    </form>
+<?php
+
+    $numero1=$_POST['valor1'];
+    $numero2=$_POST['valor'];
+        $suma=$numero1 +$numero2;
+        echo"El resultado de la Suma es : ".$suma;
+?>
+</body>
+</html>
